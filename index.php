49438e93cb270186be7f4dadef9d97ea3863a24e
Rapoo <--global>
2022-04-12 20:09:28 +0300

    Чтение изображений

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
+$images = ['jpg', 'png', 'gif'];
+
 $url = explode('/-/', $_SERVER['REQUEST_URI']);
+$format = explode('.', $_SERVER['REQUEST_URI']);
+$format = $format[count($format) - 1];
 
 if (isset($url[1])) {
+    /**
+     * Проверка для отрисовки изображения
+     */
+    if (in_array($format, $images)) {
+        header('Content-Type: image/' . $format);
+        header('Content-Length: ' . filesize($url[1]));
+    }
+
     echo file_get_contents($url[1]);
 } else {
-    echo 'Не указан get параметр url';
+    return 'Не указан get параметр url';
 }
